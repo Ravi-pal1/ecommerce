47c8625ebb2b1c4c04302e0c695e5335ef570022
Ravi Pal <ravi.pal@ucodesoft.com>
2024-05-16 19:04:45 +0530

    added auth routes setup and configured environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import { Prisma, PrismaClient } from '@prisma/client'
 import express, { Express, Request, Response } from 'express'
+import config from './config'
+import rootRouter from './routes'
 
 const app: Express = express()
+const prisma = new PrismaClient()
+
+app.use("/api", rootRouter)
 
 app.get("/", function (req: Request, res: Response) {
     res.send("Working")
 })
 
-app.listen(4000, () => console.log("Server in running on PORT 4000")
+app.listen(config.PORT, () => console.log("Server in running on PORT 4000")
 )
